7b88fafbe094ad599b370c18dc63b1cdd03c8dd6
IAMMOIZ <moizmit02@gmail.com>
2021-07-17 15:44:17 +0530

    first commit

---
+++ contoller/user/validation.js
@@ -0,0 +1,11 @@
+'use strict';
+ const Schemas = function() {
+this.userSchema = {
+
+"domainId': {
+
+notEmpty: true,
+
+errorMessage: 'domainId is mandatory'
+}
+module.exports = Schemas;
---
+++ helpers/response.js
@@ -0,0 +1,36 @@
+var responseHelper = {
+sendResponse: function(res, success, data, errors, statusCode){
+success = (success)? true : false;
+errors = errors || null;
+{ data = data || null;
+statusCode- statusCode || 1000;
+
+var obj = {
+success: success,
+errors: errors,
+status_code: statusCode
+};
+
+if (statusCode != 1002) 
+obj.data = data;
+res.json (obj);
+};
+
+formatResponse: function(errors) {
+var errorResponse = []; 
+for (var i in errors) {  
+var obj = {
+
+"location": "body",
+"parameter": 1,
+ "msg": errors[i].message
+}
+errorResponse.push(obj);
+
+}
+
+return errorResponse;
+}
+}
+
+module.exports = responseHelper;
